c414c7f10241fc0e8852964f34816ae4ee8fc5af
fvilca <fvilca@uc.cl>
2024-03-22 17:03:26 -0300

    listos los datitos

--- borrador.R
+++ borrador.R
@@ -1,2 +1,20 @@
 ### pa hacer weas 
-### pero con respeto
+### por si se nos olvida escogimos 
+### sueño	y nutricion
+
+# librerias ----
+
+library(rio)
+library(dplyr)
+library(ggplot2)
+library(patchwork)
+
+datos <- import("ENS-reducida.xlsx")
+
+str(datos)
+View(datos)
+
+datitos <- datos %>% 
+  select(c(1:6),c(27:38),c(66,76))
+
+head(datitos)
